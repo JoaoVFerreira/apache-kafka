53334081e36c9797cc4d990499a31bd1466c8825
João Ferreira <joaovtf39@gmail.com>
2023-02-11 19:46:08 -0300

    feat: add main.go

--- simulator/main.go
+++ simulator/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"log"
 	"fmt"
-
-	"github.com/JoaoVFerreira/tracking-app/application/route"
+	"github.com/JoaoVFerreira/tracking-app/infra/kafka"
+	kafka2 "github.com/JoaoVFerreira/tracking-app/application/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	route := route.Route{
-		ID:       "1",
-		ClientID: "1",
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loanding .env file")
 	}
+}
 
-	route.LoadPositions()
-	stringjson, _ := route.ExportJsonPositions()
-	fmt.Println(stringjson[0])
+func main() {
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
+	}
 }
